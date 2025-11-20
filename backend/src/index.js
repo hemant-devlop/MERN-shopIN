2ceba75a100075ba84b0e090a03511986b2a08d6
Hemantkumar <8114hd@gmail.com>
2025-11-20 23:04:33 +0530

    update corss

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,17 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://shopin001.netlify.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
